c678b48f2d3152013e434ce40335ac516c45ac7a
Yash Mittal <yash082002@gmail.com>
2025-03-12 00:33:53 -0700

    Add date conversion, styling, and random width utility to SongHistory component

--- src/Pages/SongHistory.tsx
+++ src/Pages/SongHistory.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react"
 import { supabase } from "../config/supabase"
+import { random } from "gsap";
 
 const SongHistory = () => {
     const[songData, setSongData] = useState<{ 
         song_name:string, 
-        created_at:number, 
+        created_at:string, 
         song_artists:string,
         album_cover:string
     }[]>();
@@ -19,25 +20,43 @@ const SongHistory = () => {
             }
     }
 
+    function getRandomInt(min:number, max:number) {
+        return Math.floor(Math.random() * (max - min) + min);
+      }
+
     useEffect(() => {
         showSpotifyData()
     },[])
 
 
+    //2025-03-10 03:31:03.712+00
+
+    function convertDateToPST(timeStamp:string) {
+        const date = new Date(timeStamp)
+
+        const formattedTime = date.toLocaleTimeString('en-US', { 
+            hour: 'numeric',
+            minute: '2-digit',
+            hour12: true 
+          });
+
+        return formattedTime
+    }
+
     return (
-       
-        <div>
-            <p>{songData && songData.map((currentSongs) => {
+        <div className="overflow-hidden gap-16">
+            <div className="flex flex-wrap gap-8 max-w-3xl">{songData && songData.map((currentSongs) => {
                 return (
-                    <div>
-                        <div>{currentSongs.song_name}</div>
-                        <div>{currentSongs.created_at}</div>
-                        <div>{currentSongs.song_artists}</div>
-                        <img src={currentSongs.album_cover} width="150" height="150"></img>
+                    // <div className= {`w-${getRandomInt(1, 6)}`}>
+                    <div className= 'w-20'>
+                        <img src={currentSongs.album_cover} className="w-full h-auto"></img>
+                        <div className="text-xs opacity-35 tracking-tighter">{currentSongs.song_name}</div>
+                        <div className="text-xs"> {convertDateToPST(currentSongs.created_at)}</div>
+                        <div className="text-xs">{currentSongs.song_artists}</div>
                     </div>
                 )
             })}
-            </p>
+            </div>
         </div>
     )
 }
